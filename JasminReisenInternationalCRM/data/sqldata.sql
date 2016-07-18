5dd3ac5b10fcf142a55e9884f1eb3c51d7ecb349
Mario Kellner <Mario.Kellner@studmail.w-hs.de>
2016-07-18 14:36:03 +0200

    import sql dump

---
+++ JasminReisenInternationalCRM/data/sqldata.sql
@@ -0,0 +1,25 @@
+-- --------------------------------------------------------
+-- Host:                         193.175.199.130
+-- Server Version:               5.6.28-0ubuntu0.15.10.1 - (Ubuntu)
+-- Server Betriebssystem:        debian-linux-gnu
+-- HeidiSQL Version:             9.3.0.4984
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
