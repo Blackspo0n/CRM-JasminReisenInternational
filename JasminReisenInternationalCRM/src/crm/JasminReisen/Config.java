fcad817f68d41aa820d60f07525b0fbac03b4b85
ja32759 <jan-markus.momper@studmail.w-hs.de>
2016-07-18 15:10:31 +0200

    added Config

---
+++ JasminReisenInternationalCRM/src/crm/JasminReisen/Config.java
@@ -0,0 +1,32 @@
+package crm.JasminReisen;
+
+import java.awt.Color;
+import java.awt.Font;
+
+public class Config {
+	
+	static private Color BACKGROUND = Color.blue;
+	static private Font FONT = new Font("Calibri Light", Font.PLAIN, 15);
+	static private Color FOREGROUND = Color.white;
+	
+	
+	public static Color getBACKGROUND() {
+		return BACKGROUND;
+	}
+	public static void setBACKGROUND(Color bACKGROUND) {
+		BACKGROUND = bACKGROUND;
+	}
+	public static Font getFONT() {
+		return FONT;
+	}
+	public static void setFONT(Font fONT) {
+		FONT = fONT;
+	}
+	public static Color getFOREGROUND() {
+		return FOREGROUND;
+	}
+	public static void setFOREGROUND(Color fOREGROUND) {
+		FOREGROUND = fOREGROUND;
+	}
+	
+}
--- JasminReisenInternationalCRM/src/crm/JasminReisen/GUI/MainFrame.java
+++ JasminReisenInternationalCRM/src/crm/JasminReisen/GUI/MainFrame.java
@@ -7,16 +7,20 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
+import crm.JasminReisen.Config;
+
 public class MainFrame extends JFrame {
 	
 	public MainFrame () {
 		
 		this.setSize(800, 600);
 		this.setLayout(new GridLayout(4,2,0,0));
+		this.setBackground(Config.getBACKGROUND());
 		
 		
 		//Menü Leiste
 		JMenuBar menuBar = new JMenuBar();
+		menuBar.setFont(Config.getFONT());
 		setJMenuBar(menuBar);
 		
 		// Menü Reiter
