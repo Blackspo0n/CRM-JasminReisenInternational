a679f0fe0c1e12805bdf9fc82d54bc49609c939e
ja32759 <jan-markus.momper@studmail.w-hs.de>
2016-07-18 14:35:18 +0200

    Driver + Stuff

---
+++ JasminReisenInternationalCRM/src/GUI/MainFrame.java
@@ -0,0 +1,16 @@
+package GUI;
+
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+	
+	public MainFrame () {
+		
+		this.setSize(800, 600);
+		this.setLayout(new GridLayout(8,3,0,0));
+		
+	}
+
+}
