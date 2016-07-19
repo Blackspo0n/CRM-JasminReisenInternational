06cd937a96b2c257cb98eed1c82cbe02683f4240
Mario Kellner <Mario.Kellner@studmail.w-hs.de>
2016-07-19 09:48:04 +0200

    change local variable to class attribute

--- JasminReisenInternationalCRM/src/crm/JasminReisen/GUI/LoginFrame.java
+++ JasminReisenInternationalCRM/src/crm/JasminReisen/GUI/LoginFrame.java
@@ -16,54 +16,121 @@ public class LoginFrame extends JDialog
 {
 	private JPanel contentPane;
 	private JPanel buttonPane;
-
+	private JLabel labelUser;
+	private JLabel labelPassword;
+	private JTextField txtUser;
+	private JPasswordField txtPassword;
+	private JButton loginButton;
+	private JFrame mainFrame;
+	
 	public static void main(String[] args) {
 		LoginFrame lf = new LoginFrame(new JFrame());
 	}
 	
 	public LoginFrame(JFrame parent) {
 		super(parent);
-		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setResizable(false);
 		setTitle("Anmeldung");
 		setSize(350, 114);
+		setLayout(new BorderLayout());
+		setLocationRelativeTo(null);
 		
-        Dimension windowSize = this.getSize();
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		setLocation(screenSize.width/2 - windowSize.width/2, screenSize.height/2 - windowSize.height/2);
-
-		this.setLayout(new BorderLayout());
+		mainFrame = parent;
 		
 		buttonPane = new  JPanel();
-		this.add(buttonPane, BorderLayout.SOUTH);
-		
-		contentPane =  new JPanel(new GridLayout(2,2,6,3));
-		
-		
+		contentPane = new JPanel(new GridLayout(2,2,6,3));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		this.add(contentPane, BorderLayout.CENTER);
+		
+		add(contentPane, BorderLayout.CENTER);
+		add(buttonPane, BorderLayout.SOUTH);
 				
 
-		JLabel labelUser = new JLabel();
+		labelUser = new JLabel();
 		labelUser.setText("Benutzername:");
 		contentPane.add(labelUser);
 
-		JTextField txtUser = new JTextField();
+		txtUser = new JTextField();
 		contentPane.add(txtUser);
 		
-		JLabel labelPassword = new JLabel();
+		labelPassword = new JLabel();
 		labelPassword.setText("Password:");
 		contentPane.add(labelPassword);
 
-		JPasswordField txtPassword = new JPasswordField();
+		txtPassword = new JPasswordField();
 		contentPane.add(txtPassword);	
 		
-		JButton loginButton = new JButton("Einloggen");
+		loginButton = new JButton("Einloggen");
 		buttonPane.add(loginButton);
 		
 		loginButton.addActionListener(new LoginFrameListener(this));
 		
-		this.setVisible(true);
+		setVisible(true);
 	}
+
+	public JFrame getMainFrame() {
+		return mainFrame;
+	}
+
+	public void setMainFrame(JFrame mainFrame) {
+		this.mainFrame = mainFrame;
+	}
+
+	public JPanel getContentPane() {
+		return contentPane;
+	}
+
+	public void setContentPane(JPanel contentPane) {
+		this.contentPane = contentPane;
+	}
+
+	public JPanel getButtonPane() {
+		return buttonPane;
+	}
+
+	public void setButtonPane(JPanel buttonPane) {
+		this.buttonPane = buttonPane;
+	}
+
+	public JLabel getLabelUser() {
+		return labelUser;
+	}
+
+	public void setLabelUser(JLabel labelUser) {
+		this.labelUser = labelUser;
+	}
+
+	public JLabel getLabelPassword() {
+		return labelPassword;
+	}
+
+	public void setLabelPassword(JLabel labelPassword) {
+		this.labelPassword = labelPassword;
+	}
+
+	public JTextField getTxtUser() {
+		return txtUser;
+	}
+
+	public void setTxtUser(JTextField txtUser) {
+		this.txtUser = txtUser;
+	}
+
+	public JPasswordField getTxtPassword() {
+		return txtPassword;
+	}
+
+	public void setTxtPassword(JPasswordField txtPassword) {
+		this.txtPassword = txtPassword;
+	}
+
+	public JButton getLoginButton() {
+		return loginButton;
+	}
+
+	public void setLoginButton(JButton loginButton) {
+		this.loginButton = loginButton;
+	}
+	
 }
