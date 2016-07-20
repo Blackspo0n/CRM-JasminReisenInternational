54f7e04d3002429aa664cb91e4bfc18d969d09b0
Thomas <thomas.mueller@studmail.w-hs.de>
2016-07-20 10:42:04 +0200

    Verschlüsselung erweitert

--- JasminReisenInternationalCRM/src/crm/JasminReisen/Main.java
+++ JasminReisenInternationalCRM/src/crm/JasminReisen/Main.java
@@ -3,14 +3,11 @@ package crm.JasminReisen;
 import crm.JasminReisen.Functions.DbFunctions;
 import crm.JasminReisen.GUI.MainFrame;
 
-public class Main {
-	
+public class Main {	
 	
 	public static void main(String[] args) 
-	{
-		
-		DbFunctions.connect("193.175.199.130", "CRM", "3306", "whs", "whs2016");
-		
+	{		
+		DbFunctions.connect();
 		new MainFrame();
 	}
 
