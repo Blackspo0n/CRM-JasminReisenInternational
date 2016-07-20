b55f7e2180d7c4e6a72a0b1e269dc8ad3f08c468
Thomas <thomas.mueller@studmail.w-hs.de>
2016-07-20 15:52:43 +0200

    .

--- JasminReisenInternationalCRM/src/crm/JasminReisen/Listener/CoreDataTripListener.java
+++ JasminReisenInternationalCRM/src/crm/JasminReisen/Listener/CoreDataTripListener.java
@@ -40,30 +40,41 @@ public class CoreDataTripListener implements ActionListener {
 			break;
 
 		case "Suchen":
-
 			
-			String priceStart = cdf.getTripPriceFromField().getText();
-			String priceEnd = cdf.getTripPriceToField().getText();
-			double priceEndDouble = 0;
-			double priceStartDouble = 0;
-			if (!priceStart.equals("")) {
-				priceStartDouble = Double.parseDouble(priceStart);
-				priceStart.replace(",", ".");
+			sql = "SELECT k.KlimaBeschreibung, r.*, t.* "
+					+ "FROM Klima AS k, Reisen AS r, Transportmittel as t "
+					+ "WHERE k.KlimaID = r.KlimaID AND t.TransportmittelID = r.TransportmittelID";
+					
+			if (! cdf.getTripNameField().getText().equals(""))	
+				sql += " AND r.Name LIKE '%" + cdf.getTripNameField().getText() + "%' ";
+			if (! cdf.getTripDestinatonField().getText().equals(""))	
+				sql += " AND r.Name LIKE '%" + cdf.getTripDestinatonField().getText() + "%' ";
+					
+			/*					
+					+ "AND r.Region LIKE '%" + cdf.getTripRegionField().getText() + "%' "
+					+ "AND r.Hotel LIKE '%" + cdf.getTripHotelField().getText() + "%' "
+					+ "AND r.KlimaID = '"+ cdf.getClimatesBox().getSelectedIndex() + "' AND r.KlimaID = k.KlimaID AND r.TransportID = '"
+							+ cdf.getVehicleBox().getSelectedIndex() + "' AND r.TransportID = t.TransportID AND ";
+			
+			if ((Date) cdf.getDatePickerTripStart().getModel().getValue() != null)
+				sql += "r.Reisebeginn > " + (Date) cdf.getDatePickerTripStart().getModel().getValue() + " AND ";
+			if ((Date) cdf.getDatePickerTripEnd().getModel().getValue() != null)
+				sql += "r.Reiseende < " + (Date) cdf.getDatePickerTripStart().getModel().getValue() + " AND ";
+			
+			
 			}
 			if (!priceEnd.equals("")) {
 				priceEndDouble = Double.parseDouble(priceEnd);
 				priceEnd.replace(",", ".");
 			}
-			sql = "SELECT k.KlimaBeschreibung, r.*, t.* FROM Klima AS k Reisen AS r Transportmittel as t WHERE r.Name LIKE '%"
-					+ cdf.getTripNameField().getText() + "%' AND r.Zielort LIKE '%"
-					+ cdf.getTripDestinatonField().getText() + "%' AND r.Reisebeginn > '"
+			r.Reisebeginn > '"
 					+ (Date) cdf.getDatePickerTripStart().getModel().getValue() + "' AND r.Reiseende < '"
-					+ (Date) cdf.getDatePickerTripEnd().getModel().getValue() + "' AND r.Region LIKE '%"
-					+ cdf.getTripRegionField().getText() + "%' AND r.Hotel LIKE '%" + cdf.getTripHotelField().getText()
+					+ (Date) cdf.getDatePickerTripEnd().getModel().getValue() 					
 					+ "%' AND r.Preis < '" + priceEndDouble + "' AND r.Preis > '" + priceStartDouble
-					+ "' AND Kontingent LIKE '%" + cdf.getTripQuotaField().getText() + "%' AND r.KlimaID = '"
-					+ cdf.getClimatesBox().getSelectedIndex() + "' AND r.KlimaID = k.KlimaID AND r.TransportID = '"
-					+ cdf.getVehicleBox().getSelectedIndex() + "' AND r.TransportID = t.TransportID";
+					
+					
+					+ "' AND Kontingent LIKE '%" + cdf.getTripQuotaField().getText() + "%' AND "
+			*/			
 			System.out.println(sql);
 			cdf.getCustomerTable().setModel(DbFunctions.getFilteredCustomers(sql));
 			cdf.getCustomerTable().repaint();
