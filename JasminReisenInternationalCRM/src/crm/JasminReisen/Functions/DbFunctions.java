e3c17ee6c645f3bab4106d5e3200f8939c9d1a2b
ja32759 <jan-markus.momper@studmail.w-hs.de>
2016-07-22 13:22:13 +0200

    .

--- JasminReisenInternationalCRM/src/crm/JasminReisen/Functions/DbFunctions.java
+++ JasminReisenInternationalCRM/src/crm/JasminReisen/Functions/DbFunctions.java
@@ -14,6 +14,7 @@ import java.util.List;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 
+import crm.JasminReisen.models.HistoryReise;
 import crm.JasminReisen.models.Kunde;
 import crm.JasminReisen.models.Reise;
 import crm.JasminReisen.GUI.ContactDescriptionFrame;
